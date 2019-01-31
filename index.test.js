f201c6475b9022b3ac85eab3b320e32ee5dcd084
Javier Alavrez <javieralvarez82@live.com>
2019-01-30 23:22:47 -0800

    debugging success

---
+++ index.test.js
@@ -0,0 +1,21 @@
+const item = require('./index.js');
+
+describe('success function', () => {
+
+    test('success test', () => {
+        expect(item.success(items)).toBeDefined();
+        expect(item.success(items)).toBeLessThan(21);
+        expect(item.success(items)).
+        expect(item.success(items)).
+    })
+}) 
+
+
+
+
+const items = {
+        name: "bastard sword",
+        type: "weapon",
+        durability: 100,
+        enhancement: "+ " + 20,
+    }
