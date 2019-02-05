e4d2aa70c506c7db93ede14a5aa46267223ca77e
Javier Alavrez <javieralvarez82@live.com>
2019-02-04 23:06:09 -0800

    updated fail condition

--- index.js
+++ index.js
@@ -52,72 +52,60 @@ success = (item) => {
 }
 
 fail = (item) => {
-
     
-    fail = (item) => {
-    
-   
-        let durabilityDamage = item.durability - 5;
-        let enhancedDamage = item.durability - 10;
-        let negEnhanceLevel = item.enhancement - 1;
-      
-        if(item.enhancement <= 14) {
-                
-                item.durability = durabilityDamage - 5;
-                item.name = `[+${item.enhancement}] ${item.name}`;
-            }
-     
-          else if(item.enhancement === 16) {
-                item.durability = durabilityDamage - 10;
-                item.name = `[+${item.enhancement}] ${item.name}`
-                }
-     
-          else if(item.enhancement > 16) {
-               item.enhancement = negEnhanceLevel
-               item.durability = enhancedDamage
-               item.name = `[+${item.enhancement}] ${item.name}`
-          }
-         
-        
-          else(item.enhancement >= 20) 
-                const error = "Weapon is at max"
-                 error
-             
-     
-     let level = item;
-     
-      eLevel = (level) => {
-             const swordLevels = [
-                 "PRI",
-                 "DUO",
-                 "TRI",
-                 "TET",
-                 "PEN"
-             ]
-            const swordLevel = level.enhancement;
-     
-             if(swordLevel === 16) {
-                 return level.enhancement = swordLevels[0];
-             }
-             else if(swordLevel === 17) {
-                 level.enhancement = swordLevels[1];
-             }
-             else if(swordLevel === 18) {
-                 level.enhancement = swordLevels[2];
-             }
-             else if(swordLevel === 19) {
-                 level.enhancement = swordLevels[3];
-             }
-             else if(swordLevel === 20) {
-                level.enhancement = swordLevels[4];
-             }
-         
+    let durabilityDamage = item.durability - 5;
+    let enhancedDamage = item.durability - 10;
+    let negEnhanceLevel = item.enhancement - 1;
+  
+    if(item.enhancement <= 14) {
+            item.durability = durabilityDamage;
+            item.name = `[+${item.enhancement}] ${item.name}`;
+        }
+ 
+      else if(item.enhancement === 16) {
+            item.durability = durabilityDamage;
+            item.name = `[+${item.enhancement}] ${item.name}`
+        }
+ 
+      else if(item.enhancement > 16) {
+           item.enhancement = negEnhanceLevel
+           item.durability = enhancedDamage
+           item.name = `[+${item.enhancement}] ${item.name}`
+        }
+ 
+  eLevel = (item) => {
+         const swordLevels = [
+             "PRI",
+             "DUO",
+             "TRI",
+             "TET",
+             "PEN"
+         ]
+        const swordLevel = item.enhancement;
+ 
+         if(swordLevel === 16) {
+              item.enhancement = swordLevels[0];
          }
-         eLevel(level);
-         
-           return item //end of eLevel
+         else if(swordLevel === 17) {
+             item.enhancement = swordLevels[1];
+         }
+         else if(swordLevel === 18) {
+             item.enhancement = swordLevels[2];
+         }
+         else if(swordLevel === 19) {
+             item.enhancement = swordLevels[3];
+         }
+         else if(swordLevel === 20) {
+            item.enhancement = swordLevels[4];
+         }
+     
      }
+ 
+     eLevel(item);
+ 
+       return item //end of eLevel
 }
+
 
 repair = (item) => {
 
